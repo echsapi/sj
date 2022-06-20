477b493d3fc3f2473db7403f8097e16b8ab60cca
echsapi <90782367+echsapi@users.noreply.github.com>
2022-06-21 01:20:33 +0800

    Update index.php
    up

--- api/index.php
+++ api/index.php
@@ -1,53 +1,4 @@
 <?php
-
-const ALLOW_OUTPUT = false; // 修改以开启
-
-const ERROR_IMG = [
-
-    404 => 'https://http.cat/404',
-
-    503 => 'https://http.cat/503'
-
- ];
-
-if (file_exists('../url.csv')) {
-
-    $url = file('../url.csv', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-
-} else {
-
-    $url = file('http://' . $_SERVER['HTTP_HOST'] . '/url.csv', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-
-}
-
-if (empty($url[0])) {
-
-    $code = 503;
-
-    $target_url = ERROR_IMG[503];
-
-}
-
-$id         = $_REQUEST['id'];
-
-$type       = $_REQUEST['type'];
-
-$length    = count($url);
-
-$final_id   = array_rand($url);
-
-$is_idValid = false;
-
-if (isset($id)) {
-
-    header('Cache-Control: public, max-age=86400');
-
-    if (is_numeric($id)) {
-
-        settype($id, 'integer');
-
-        if ($is_idValid = is_int($id)) {
-<?php
 const ALLOW_OUTPUT = false; // 修改以开启
 const ERROR_IMG = [
     404 => 'https://http.cat/404',
@@ -142,23 +93,3 @@ switch ($type) {
     default:
         header('Location: ' . $target_url);
 }
-
-            $final_id = $id;  // id是整数
-
-        }
-
-    }
-
-} else {
-
-    header('Cache-Control: no-cache');
-
-}
-
-if (!$code && $is_idValid && $final_id > $length) {
-
-    // exceed maximum length
-
-    $code       = 404;
-
-    $target_url = ERROR_
